091ba26a4ff6ceb3c4f40dd4a8744824b3da63fa
Globox1997 <mitschke11@web.de>
2020-08-07 20:57:27 -0500

    added piglin brain tag

--- src/main/java/net/adventurez/init/TagInit.java
+++ src/main/java/net/adventurez/init/TagInit.java
@@ -2,6 +2,7 @@ package net.adventurez.init;
 
 import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 
@@ -10,6 +11,8 @@ public class TagInit {
   public static final Tag<Block> UNBREAKABLE_BLOCKS = TagRegistry
       .block(new Identifier("adventurez", "unbreakable_blocks"));
   public static final Tag<Block> PLATFORM_BLOCKS = TagRegistry.block(new Identifier("adventurez", "platform_blocks"));
+  public static final Tag<Item> PIGLIN_NOT_ATTACK = TagRegistry
+      .item(new Identifier("adventurez", "piglin_not_attack_items"));
 
   public static void init() {
   }
---
+++ src/main/java/net/adventurez/mixin/PiglinBrainMixin.java
@@ -0,0 +1,27 @@
+package net.adventurez.mixin;
+
+import java.util.Iterator;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.At;
+
+import net.adventurez.init.TagInit;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.PiglinBrain;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+@Mixin(PiglinBrain.class)
+public class PiglinBrainMixin {
+
+  @Inject(method = "wearsGoldArmor(Lnet/minecraft/entity/LivingEntity;)Z", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;"), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+  private static void wearsGoldArmorMixin(LivingEntity entity, CallbackInfoReturnable<Boolean> cir,
+      Iterable<ItemStack> iterable, Iterator<ItemStack> iterator, ItemStack stack, Item item) {
+    if (item.isIn(TagInit.PIGLIN_NOT_ATTACK))
+      cir.setReturnValue(true);
+  }
+
+}
