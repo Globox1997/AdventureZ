817fdcb0e88245abf91773da29cfc4d33eb44b2c
Globox1997 <mitschke11@web.de>
2020-09-22 16:01:04 +0200

    added shield+teleport spell

--- src/main/java/net/adventurez/entity/SpellCastingEntity.java
+++ src/main/java/net/adventurez/entity/SpellCastingEntity.java
@@ -91,7 +91,8 @@ public abstract class SpellCastingEntity extends HostileEntity {
   }
 
   public static enum Spell {
-    NONE(0, 0.0D, 0.0D, 0.0D), SUMMON_PUPPET(1, 0.01D, 0.01D, 0.015D), WITHERING(2, 0.01D, 0.01D, 0.015D);
+    NONE(0, 0.0D, 0.0D, 0.0D), SUMMON_PUPPET(1, 0.01D, 0.01D, 0.015D), WITHERING(2, 0.01D, 0.01D, 0.015D),
+    SHIELD(3, 0.01D, 0.01D, 0.015D), TELEPORT(4, 0.01D, 0.01D, 0.015D);
 
     private final int id;
     private final double[] particleVelocity;
