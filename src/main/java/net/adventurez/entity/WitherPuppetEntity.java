ef5afded7a215831de5863f9ab629d4ff2729a8b
Globox1997 <mitschke11@web.de>
2020-09-22 16:01:47 +0200

    added wander around goal

--- src/main/java/net/adventurez/entity/WitherPuppetEntity.java
+++ src/main/java/net/adventurez/entity/WitherPuppetEntity.java
@@ -11,6 +11,7 @@ import net.minecraft.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.entity.ai.goal.RevengeGoal;
 import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.ai.goal.TrackTargetGoal;
+import net.minecraft.entity.ai.goal.WanderAroundGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
@@ -50,6 +51,7 @@ public class WitherPuppetEntity extends HostileEntity {
     super.initGoals();
     this.goalSelector.add(0, new SwimGoal(this));
     this.goalSelector.add(1, new MeleeAttackGoal(this, 1.2D, false));
+    this.goalSelector.add(6, new WanderAroundGoal(this, 0.9D));
     this.goalSelector.add(7, new LookAtEntityGoal(this, PlayerEntity.class, 3.0F, 1.0F));
     this.goalSelector.add(8, new LookAtEntityGoal(this, MobEntity.class, 8.0F));
     this.targetSelector.add(1, (new RevengeGoal(this, new Class[] { NecromancerEntity.class })));
