6d4016a05608dfcc4328e4cb11679495b7e8b1d3
Globox1997 <mitschke11@web.de>
2020-11-11 20:12:55 +0100

    flush

--- src/main/java/net/adventurez/mixin/PiglinBrainMixin.java
+++ src/main/java/net/adventurez/mixin/PiglinBrainMixin.java
@@ -18,10 +18,10 @@ import net.minecraft.item.ItemStack;
 public class PiglinBrainMixin {
 
   @Inject(method = "wearsGoldArmor(Lnet/minecraft/entity/LivingEntity;)Z", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;"), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
-  private static void wearsGoldArmorMixin(LivingEntity entity, CallbackInfoReturnable<Boolean> cir,
+  private static void wearsGoldArmorMixin(LivingEntity entity, CallbackInfoReturnable<Boolean> info,
       Iterable<ItemStack> iterable, Iterator<ItemStack> iterator, ItemStack stack, Item item) {
     if (item.isIn(TagInit.PIGLIN_NOT_ATTACK))
-      cir.setReturnValue(true);
+      info.setReturnValue(true);
   }
 
 }
