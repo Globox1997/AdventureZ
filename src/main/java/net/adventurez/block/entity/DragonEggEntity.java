adad6f3a69e67b43e4195a8ccc047b0e2accc507
Globox1997 <mitschke11@web.de>
2021-03-13 23:28:53 +0100

    egg edit

--- src/main/java/net/adventurez/block/entity/DragonEggEntity.java
+++ src/main/java/net/adventurez/block/entity/DragonEggEntity.java
@@ -1,24 +1,53 @@
 package net.adventurez.block.entity;
 
+import java.util.List;
+
+import net.adventurez.entity.DragonEntity;
 import net.adventurez.entity.TheEyeEntity;
 import net.adventurez.init.BlockInit;
 import net.adventurez.init.ConfigInit;
+import net.adventurez.init.EffectInit;
 import net.adventurez.init.EntityInit;
 import net.adventurez.init.TagInit;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
-//import net.minecraft.nbt.CompoundTag;
-//import net.minecraft.particle.ParticleTypes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.predicate.entity.EntityPredicates;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Tickable;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 
 public class DragonEggEntity extends BlockEntity implements Tickable {
-    private int overallTick;
+    private int overallSummoningTick;
+    private int overallHatchingTick;
+    private int hatchTick;
+    private int summoningTick;
+    private boolean isHatchAble;
 
     public DragonEggEntity() {
         super(BlockInit.DRAGON_EGG_ENTITY);
+    }
+
+    @Override
+    public void fromTag(BlockState state, CompoundTag tag) {
+        super.fromTag(state, tag);
+        hatchTick = tag.getInt("Hatch_Tick");
+        isHatchAble = tag.getBoolean("Hatch_Able");
+        summoningTick = tag.getInt("Summoning_Tick");
+    }
+
+    @Override
+    public CompoundTag toTag(CompoundTag tag) {
+        super.toTag(tag);
+        tag.putBoolean("hatchAble", isHatchAble);
+        tag.putInt("Hatch_Tick", hatchTick);
+        tag.putInt("Summoning_Tick", summoningTick);
+        return tag;
     }
 
     @Override
@@ -71,36 +100,69 @@ public class DragonEggEntity extends BlockEntity implements Tickable {
 
     @Override
     public void tick() {
-        this.update();
+        this.updateTheEyeSummoning();
+        this.updateDragonHatching();
     }
 
-    private void update() {
+    private void updateDragonHatching() {
+        if (ConfigInit.CONFIG.allow_dragon_hatching && !this.world.isClient) {
+            overallHatchingTick++;
+            if (overallHatchingTick == 20) {
+                if (!this.isHatchAble) {
+                    Box box = new Box(this.getPos());
+                    List<PlayerEntity> list = world.getEntitiesByClass(PlayerEntity.class, box.expand(2D),
+                            EntityPredicates.EXCEPT_SPECTATOR);
+                    for (int i = 0; i < list.size(); ++i) {
+                        PlayerEntity playerEntity = (PlayerEntity) list.get(i);
+                        if (playerEntity instanceof PlayerEntity && playerEntity.hasStatusEffect(EffectInit.FAME)) {
+                            this.isHatchAble = true;
+                        }
+                    }
+                } else {
+                    this.hatchTick++;
+                    if (this.hatchTick % 60 == 0) {
+                        // for (int i = 0; i < 20; i++) {
+                        double d = (double) pos.getX() + (double) world.random.nextFloat();
+                        double e = (double) pos.getY() + (double) world.random.nextFloat();
+                        double f = (double) pos.getZ() + (double) world.random.nextFloat();
+                        ((ServerWorld) this.world).spawnParticles(ParticleTypes.HAPPY_VILLAGER, d, e, f, 0, 0.0D, 0.0D,
+                                0.0D, 0.01D);
+                        // }
+                    }
+                    if (this.hatchTick >= 598) {
+                        world.breakBlock(this.pos, false);
+                        DragonEntity dragonEntity = (DragonEntity) EntityInit.DRAGON_ENTITY.create(world);
+                        dragonEntity.refreshPositionAndAngles((double) this.getPos().getX() + 0.5D,
+                                (double) this.getPos().getY() + 0.55D, (double) this.getPos().getZ() + 0.5D, 90F, 0.0F);
+                        world.spawnEntity(dragonEntity);
+                    }
+                }
+
+            }
+            if (overallHatchingTick >= 20) {
+                overallHatchingTick = 0;
+            }
+        }
+    }
+
+    private void updateTheEyeSummoning() {
         if (this.world.getRegistryKey() == World.END
                 && this.world.getBlockState(pos.down()).equals(Blocks.CRYING_OBSIDIAN.getDefaultState())) {
-            overallTick++;
+            overallSummoningTick++;
             BlockState state = this.getCachedState();
-            // if (world.isClient) {
-            // for (int i = 0; i < 20; i++) {
-            // world.addParticle(ParticleTypes.ENCHANT, (double) this.getPos().getX() +
-            // 0.5D,
-            // (double) this.getPos().getY() + 3.0D, (double) this.getPos().getZ() + 0.5D,
-            // (double) ((float) world.random.nextFloat()) - 0.5D,
-            // (double) ((float) world.random.nextFloat() - 2.0F),
-            // (double) ((float) world.random.nextFloat()) - 0.5D);
-            // }
+            if (ConfigInit.CONFIG.allow_the_eye_summoning && overallSummoningTick == 20
+                    && this.isValid(world, pos, state)) {
+                summoningTick++;
+                if (world.isClient && summoningTick % 10 == 0) {
+                    // for (int i = 0; i < 20; i++) {
+                    double d = (double) pos.getX() + (double) world.random.nextFloat();
+                    double e = (double) pos.getY() + (double) world.random.nextFloat() + 1D;
+                    double f = (double) pos.getZ() + (double) world.random.nextFloat();
+                    world.addParticle(ParticleTypes.END_ROD, d, e, f, 0.0D, 1.0D, 0.0D);
+                    // }
 
-            // }
-            if (ConfigInit.CONFIG.allow_the_eye_summoning && overallTick == 100 && this.isValid(world, pos, state)) {
-                // if (world.isClient) {
-                // for (int i = 0; i < 20; i++) {
-                // double d = (double) pos.getX() + (double) world.random.nextFloat();
-                // double e = (double) pos.getY() + (double) world.random.nextFloat() + 1D;
-                // double f = (double) pos.getZ() + (double) world.random.nextFloat();
-                // world.addParticle(ParticleTypes.HAPPY_VILLAGER, d, e, f, 0.0D, 1.0D, 0.0D);
-                // }
-
-                // }
-                if (!world.isClient) {// && this.world.isReceivingRedstonePower(pos)
+                }
+                if (!world.isClient && summoningTick >= 60) {
                     TheEyeEntity theEyeEntity = (TheEyeEntity) EntityInit.THEEYE_ENTITY.create(world);
                     theEyeEntity.refreshPositionAndAngles((double) this.getPos().getX() + 0.5D,
                             (double) this.getPos().getY() + 0.55D, (double) this.getPos().getZ() + 0.5D, 90F, 0.0F);
@@ -109,8 +171,8 @@ public class DragonEggEntity extends BlockEntity implements Tickable {
                     this.world.breakBlock(pos, false);
                 }
             }
-            if (overallTick >= 100) {
-                overallTick = 0;
+            if (overallSummoningTick >= 20) {
+                overallSummoningTick = 0;
             }
 
         }
