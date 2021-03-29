4a7e66f965f1956eeb962ecc8eee77cf17d3f200
Globox1997 <mitschke11@web.de>
2021-03-29 12:03:02 +0200

    fixed fame effect removal

---
+++ src/main/java/net/adventurez/mixin/PlayerManagerMixin.java
@@ -0,0 +1,35 @@
+package net.adventurez.mixin;
+
+import java.util.Optional;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.At;
+
+import net.adventurez.init.EffectInit;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.network.ServerPlayerInteractionManager;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+
+@Mixin(PlayerManager.class)
+public class PlayerManagerMixin {
+
+    @Inject(method = "respawnPlayer", at = @At(value = "TAIL"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void respawnPlayerMixinSS(ServerPlayerEntity player, boolean alive,
+            CallbackInfoReturnable<ServerPlayerEntity> info, BlockPos blockPos, float f, boolean bl,
+            ServerWorld serverWorld, Optional<Vec3d> optional2,
+            ServerPlayerInteractionManager serverPlayerInteractionManager2, ServerWorld serverWorld2,
+            ServerPlayerEntity serverPlayerEntity) {
+        if (alive && player.hasStatusEffect(EffectInit.FAME)) {
+            serverPlayerEntity.addStatusEffect(new StatusEffectInstance(EffectInit.FAME,
+                    player.getStatusEffect(EffectInit.FAME).getDuration(), 0, false, false, true));
+        }
+    }
+
+}
