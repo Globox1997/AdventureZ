90333f55de3852b7b161fe54ec8be610485f6e8c
Globox1997 <mitschke11@web.de>
2021-05-20 15:43:26 +0200

    reduced prime eye damage

---
+++ src/main/java/net/adventurez/mixin/EnderPearlEntityMixin.java
@@ -0,0 +1,31 @@
+package net.adventurez.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.At;
+
+import net.adventurez.init.ItemInit;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
+import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.world.World;
+
+@Mixin(EnderPearlEntity.class)
+public abstract class EnderPearlEntityMixin extends ThrownItemEntity {
+
+    public EnderPearlEntityMixin(EntityType<? extends ThrownItemEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "onCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"), cancellable = true)
+    protected void onCollisionMixin(HitResult hitResult, CallbackInfo info) {
+        if (this.getItem().getItem() == ItemInit.PRIME_EYE) {
+            this.getOwner().damage(DamageSource.FALL, 2.0F);
+            this.remove();
+            info.cancel();
+        }
+    }
+}
