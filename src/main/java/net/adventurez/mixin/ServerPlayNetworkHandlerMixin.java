6ffa16dc82029a1d83a21cda866a530975ba8583
Globox1997 <mitschke11@web.de>
2021-05-31 15:18:48 +0200

    flying kick fix

--- src/main/java/net/adventurez/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/net/adventurez/mixin/ServerPlayNetworkHandlerMixin.java
@@ -3,6 +3,7 @@ package net.adventurez.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -17,10 +18,20 @@ public class ServerPlayNetworkHandlerMixin {
     @Shadow
     public ServerPlayerEntity player;
 
+    @Shadow
+    private int vehicleFloatingTicks;
+
     @Inject(method = "onClientCommand", at = @At(value = "TAIL"))
     public void onClientCommandMixin(ClientCommandC2SPacket packet, CallbackInfo info) {
         if (this.player.getVehicle() instanceof DragonEntity && ((DragonEntity) this.player.getVehicle()).hasChest()) {
             ((DragonEntity) this.player.getVehicle()).openInventory(this.player);
+        }
+    }
+
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getPrimaryPassenger()Lnet/minecraft/entity/Entity;", shift = Shift.AFTER, opcode = 1))
+    private void tickMixin(CallbackInfo info) {
+        if (vehicleFloatingTicks >= 70 && player.getVehicle() != null && player.getVehicle() instanceof DragonEntity) {
+            vehicleFloatingTicks = 0;
         }
     }
 
