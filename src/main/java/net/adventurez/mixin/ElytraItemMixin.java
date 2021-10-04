bb3df2fbd917f34960a5b1122a641ff65bcba3c6
Globox1997 <mitschke11@web.de>
2021-10-04 12:34:13 +0200

    added ender whale skin elytra repair

---
+++ src/main/java/net/adventurez/mixin/ElytraItemMixin.java
@@ -0,0 +1,20 @@
+package net.adventurez.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.adventurez.init.ItemInit;
+import net.minecraft.item.ElytraItem;
+import net.minecraft.item.ItemStack;
+
+@Mixin(ElytraItem.class)
+public class ElytraItemMixin {
+
+    @Inject(method = "canRepair", at = @At("HEAD"), cancellable = true)
+    private void canRepairMixin(ItemStack stack, ItemStack ingredient, CallbackInfoReturnable<Boolean> info) {
+        if (ingredient.isOf(ItemInit.ENDER_WHALE_LEATHER))
+            info.setReturnValue(true);
+    }
+}
