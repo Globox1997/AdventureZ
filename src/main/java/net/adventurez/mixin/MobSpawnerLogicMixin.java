af6404332401979e5cd8471979137f2699487cee
Fx Morin <fx.e.morin@gmail.com>
2022-03-04 15:52:39 -0500

    Added optional
    You didn't see anything

--- src/main/java/net/adventurez/mixin/MobSpawnerLogicMixin.java
+++ src/main/java/net/adventurez/mixin/MobSpawnerLogicMixin.java
@@ -1,5 +1,6 @@
 package net.adventurez.mixin;
 
+import java.util.Optional;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
