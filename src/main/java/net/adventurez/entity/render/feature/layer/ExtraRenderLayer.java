310de51de75277ba6d4fb245758bfb3c000108d6
Globox1997 <mitschke11@web.de>
2022-03-12 11:10:45 +0100

    moved to irisapi

--- src/main/java/net/adventurez/entity/render/feature/layer/ExtraRenderLayer.java
+++ src/main/java/net/adventurez/entity/render/feature/layer/ExtraRenderLayer.java
@@ -1,8 +1,8 @@
 package net.adventurez.entity.render.feature.layer;
 
 import net.fabricmc.api.Environment;
+import net.irisshaders.iris.api.v0.IrisApi;
 import net.adventurez.init.RenderInit;
-import net.coderbot.iris.Iris;
 import net.fabricmc.api.EnvType;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.Identifier;
@@ -10,7 +10,7 @@ import net.minecraft.util.Identifier;
 @Environment(EnvType.CLIENT)
 public class ExtraRenderLayer {
     public static RenderLayer getGlowing(Identifier identifier) {
-        if (RenderInit.isIrisLoaded && Iris.getIrisConfig().areShadersEnabled() && Iris.getCurrentPack().isPresent()) {
+        if (RenderInit.isIrisLoaded && IrisApi.getInstance().isShaderPackInUse()) {
             return RenderLayer.getEyes(identifier);
         } else if (RenderInit.isCanvasLoaded)
             return RenderLayer.getEntityDecal(identifier);
