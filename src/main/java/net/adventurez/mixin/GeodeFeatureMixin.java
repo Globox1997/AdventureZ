e042b3eef37c96fdcb7318cf29ed369034e33361
Globox1997 <mitschke11@web.de>
2022-06-26 15:57:50 +0200

    removed init

--- src/main/java/net/adventurez/mixin/GeodeFeatureMixin.java
+++ src/main/java/net/adventurez/mixin/GeodeFeatureMixin.java
@@ -8,7 +8,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.adventurez.entity.AmethystGolemEntity;
 import net.adventurez.init.ConfigInit;
 import net.adventurez.init.EntityInit;
-import net.minecraft.entity.SpawnReason;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.gen.feature.GeodeFeature;
 import net.minecraft.world.gen.feature.GeodeFeatureConfig;
@@ -19,14 +18,13 @@ public class GeodeFeatureMixin {
 
     @Inject(method = "generate", at = @At(value = "RETURN"))
     private void generateMixin(FeatureContext<GeodeFeatureConfig> context, CallbackInfoReturnable<Boolean> info) {
-        if (info.getReturnValue()) {
+        if (!context.getWorld().isClient() && info.getReturnValue()) {
             int amethystSpawnChance = ConfigInit.CONFIG.amethyst_golem_spawn_chance;
-            if (!context.getWorld().isClient() && amethystSpawnChance != 0) {
+            if (amethystSpawnChance != 0) {
                 int spawnChanceInt = context.getRandom().nextInt(amethystSpawnChance) + 1;
                 BlockPos spawnPos = context.getOrigin().south(4).east(4);
                 if (spawnChanceInt == 1 && (context.getWorld().isAir(spawnPos) || context.getWorld().isAir(spawnPos.up()))) {
                     AmethystGolemEntity amethystGolemEntity = (AmethystGolemEntity) EntityInit.AMETHYST_GOLEM_ENTITY.create(context.getWorld().toServerWorld());
-                    amethystGolemEntity.initialize(context.getWorld().toServerWorld(), context.getWorld().getLocalDifficulty(spawnPos), SpawnReason.STRUCTURE, null, null);
                     amethystGolemEntity.getDataTracker().set(AmethystGolemEntity.DEEPSLATE_VARIANT, spawnPos.getY() <= 0);
                     if (!context.getWorld().isAir(spawnPos))
                         spawnPos = spawnPos.up();
