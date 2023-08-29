1272e033e084d37eaab0984baed394aa97b323d6
Globox1997 <mitschke11@web.de>
2023-08-29 15:38:57 +0200

    moved to anvil

--- src/main/java/net/adventurez/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/net/adventurez/mixin/AnvilScreenHandlerMixin.java
@@ -3,26 +3,48 @@ package net.adventurez.mixin;
 import java.util.Iterator;
 import java.util.Map;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import net.adventurez.init.ItemInit;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.screen.AnvilScreenHandler;
+import net.minecraft.screen.ForgingScreenHandler;
+import net.minecraft.screen.Property;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.ScreenHandlerType;
 
 @Mixin(AnvilScreenHandler.class)
-public class AnvilScreenHandlerMixin {
+public abstract class AnvilScreenHandlerMixin extends ForgingScreenHandler {
 
     @Unique
     private int enchantmentLevel = 0;
     @Unique
     private boolean isVoidShadowDrop = false;
+    @Unique
+    private int repairedAmount;
+
+    @Shadow
+    @Mutable
+    @Final
+    private Property levelCost = Property.create();
+
+    public AnvilScreenHandlerMixin(ScreenHandlerType<?> type, int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
+        super(type, syncId, playerInventory, context);
+    }
 
     @SuppressWarnings("rawtypes")
     @Inject(method = "updateResult", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/enchantment/Enchantment;getMaxLevel()I", ordinal = 0), locals = LocalCapture.CAPTURE_FAILSOFT)
@@ -42,6 +64,45 @@ public class AnvilScreenHandlerMixin {
             return enchantmentLevel;
         } else {
             return original;
+        }
+    }
+
+    @Inject(method = "updateResult", at = @At("HEAD"), cancellable = true)
+    private void updateResultMixin(CallbackInfo info) {
+        ItemStack itemStack = this.input.getStack(0);
+        if (itemStack.getItem() == ItemInit.PRIME_EYE && itemStack.getDamage() != 0) {
+            ItemStack itemStack2 = this.input.getStack(1);
+            if (itemStack2.getItem() == Items.ENDER_PEARL) {
+                ItemStack itemStack3 = new ItemStack(ItemInit.PRIME_EYE);
+                repairedAmount = itemStack2.getCount() > itemStack.getDamage() ? itemStack.getDamage() : itemStack2.getCount();
+                itemStack3.setDamage(itemStack.getDamage() - repairedAmount);
+                this.output.setStack(0, itemStack3);
+                info.cancel();
+            }
+        }
+    }
+
+    @Inject(method = "canTakeOutput", at = @At("HEAD"), cancellable = true)
+    public void canTakeOutputMixin(PlayerEntity player, boolean present, CallbackInfoReturnable<Boolean> info) {
+        ItemStack itemStack = this.input.getStack(0);
+        if (itemStack.getItem() == ItemInit.PRIME_EYE && itemStack.getDamage() != 0) {
+            ItemStack itemStack2 = this.input.getStack(1);
+            if (itemStack2.getItem() == Items.ENDER_PEARL) {
+                info.setReturnValue(true);
+            }
+        }
+    }
+
+    @Inject(method = "onTakeOutput", at = @At("HEAD"))
+    public void onTakeOutputMixin(PlayerEntity player, ItemStack stack, CallbackInfo info) {
+        ItemStack itemStack = this.input.getStack(0);
+        if (itemStack.getItem() == ItemInit.PRIME_EYE) {
+            ItemStack itemStack2 = this.input.getStack(1);
+            if (itemStack2.getItem() == Items.ENDER_PEARL) {
+                itemStack2.decrement(repairedAmount - 1);
+                this.output.setStack(0, itemStack2);
+                this.levelCost.set(0);
+            }
         }
     }
 
